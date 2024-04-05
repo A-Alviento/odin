a38694885358f74e3fbcd004ceb11d6436d600e8
Adrian Alviento <adrianalviento@activate.sg>
2024-04-05 16:34:45 +0800

    Finish js-exercise 6

--- javascript-exercises-main/06_leapYears/leapYears.js
+++ javascript-exercises-main/06_leapYears/leapYears.js
@@ -1,5 +1,14 @@
-const leapYears = function() {
+const leapYears = function (year) {
+  const isLeapYear =
+    year % 4 === 0
+      ? !(year % 100 === 0)
+        ? true
+        : year % 400 === 0
+        ? true
+        : false
+      : false;
 
+  return isLeapYear;
 };
 
 // Do not edit below this line
