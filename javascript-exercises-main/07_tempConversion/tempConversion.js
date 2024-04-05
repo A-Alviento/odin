b05f84f9effbd974ad22dd960d1bc9dbd40288c4
Adrian Alviento <adrianalviento@activate.sg>
2024-04-05 16:42:31 +0800

    Finish js-exercise 7

--- javascript-exercises-main/07_tempConversion/tempConversion.js
+++ javascript-exercises-main/07_tempConversion/tempConversion.js
@@ -1,11 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (tempFahrenheit) {
+  tempCelsius = ((tempFahrenheit - 32) * 5) / 9;
+  return Number(tempCelsius.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (tempCelsius) {
+  tempFahrenheit = (tempCelsius * 9) / 5 + 32;
+  return Number(tempFahrenheit.toFixed(1));
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
