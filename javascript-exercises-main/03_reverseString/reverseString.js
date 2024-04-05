ee250d65baa04588ea6e62950251ea611f58dbfe
Adrian Alviento <adrianalviento@activate.sg>
2024-04-05 16:09:43 +0800

    Finish js-exercise 03

--- javascript-exercises-main/03_reverseString/reverseString.js
+++ javascript-exercises-main/03_reverseString/reverseString.js
@@ -1,6 +1,14 @@
-const reverseString = function() {
+const reverseString = function (word) {
+  reverseWord = "";
 
+  for (let i = 0; i < word.length; i++) {
+    reverseWord += word[word.length - i - 1];
+  }
+
+  return reverseWord;
 };
+
+reverseString("hello");
 
 // Do not edit below this line
 module.exports = reverseString;
