629d268ab2d8c8f1d3246350df15a715cf9d5802
Adrian Alviento <adrianalviento@activate.sg>
2024-04-08 15:57:10 +0800

    Finish js/foundations/dom/exercise

--- javascript/foundations-dom/script.js
+++ javascript/foundations-dom/script.js
@@ -5,3 +5,26 @@ content.classList.add("content");
 content.textContent = "This is the glorious text-content!";
 
 container.appendChild(content);
+
+const paragraph = document.createElement("p");
+paragraph.textContent = "Hey I'm red!";
+paragraph.style.color = "red";
+
+container.appendChild(paragraph);
+
+const headerThree = document.createElement("h3");
+headerThree.textContent = "I'm a blue h3!";
+headerThree.style.color = "blue";
+
+container.appendChild(headerThree);
+
+const headerOne = document.createElement("h1");
+headerOne.textContent = "I'm in a div";
+const paragraphTwo = document.createElement("p");
+paragraphTwo.textContent = "ME TOO!";
+const div = document.createElement("div");
+div.textContent = "";
+div.appendChild(headerOne);
+div.appendChild(paragraphTwo);
+
+container.appendChild(div);
