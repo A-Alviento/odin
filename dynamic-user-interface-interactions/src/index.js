a40706d3db05b6844dc6bea36b4ed18f27d5e389
Adrian Alviento <adrianalviento@activate.sg>
2024-06-11 20:07:32 +0800

    Start on carousel

--- dynamic-user-interface-interactions/src/index.js
+++ dynamic-user-interface-interactions/src/index.js
@@ -1,27 +1,28 @@
 import "./reset.css";
-import "./style.css";
+import "./style-dropdown.css";
+import "./style-carousel.css";
 
 import { dropdown } from "./dropdown-ui";
 
 const domContent = document.querySelector("#content");
 
-domContent.appendChild(
-  dropdown({
-    title: "Select an option",
-    options: [
-      "option1",
-      "option2",
-      "option3",
-      "option4",
-      "option5",
-      "option1",
-      "option2",
-      "option3",
-      "option4",
-      "option5",
-    ],
-  })
-);
+// domContent.appendChild(
+//   dropdown({
+//     title: "Select an option",
+//     options: [
+//       "option1",
+//       "option2",
+//       "option3",
+//       "option4",
+//       "option5",
+//       "option1",
+//       "option2",
+//       "option3",
+//       "option4",
+//       "option5",
+//     ],
+//   })
+// );
 // domContent.appendChild(
 //   dropdown({
 //     title: "Select an option",
